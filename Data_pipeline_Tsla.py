d6aed193bfa0bc3962e38b94962c60b376ba79e0
sfdatade <sfdatade@gmail.com>
2023-07-20 14:16:50 +0200

    API_Pipeline_a4

--- Data_pipeline_Tsla.py
+++ Data_pipeline_Tsla.py
@@ -1,11 +1,30 @@
 import urllib3
 import json
+
 def get_data():
-    http = urllib3.PoolManager()
-    url = "https://cloud.iexapis.com/stable/stock/tsla/previous?token=<pk_99f064fa01784e02a0e518683fea021f>"
+    # Disable SSL certificate verification (Not recommended for production)
+    http = urllib3.PoolManager(cert_reqs='CERT_NONE')
+
+    # Read token from file
+    with open("token.txt", "r") as file:
+        token = file.read().strip()
+
+    url = f"https://cloud.iexapis.com/stable/stock/tsla/previous?token={token}"
     resp = http.request("GET", url)
-    values = json.loads(resp.data)
-    print(values)
-    return values
+
+    # Decode the response data and print it
+    data = resp.data.decode("utf-8")
+    print("Response Data:", data)
+
+    try:
+        values = json.loads(data)
+        print(values)
+        return values
+    except json.JSONDecodeError as e:
+        print("JSON Parsing Error:", e)
+        return None
+
 get_data()
+
+
 
