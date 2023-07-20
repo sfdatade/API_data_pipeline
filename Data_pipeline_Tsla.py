36789a74f20198c75e1ceeaf0e522e827625c46d
sfdatade <sfdatade@gmail.com>
2023-07-20 11:51:18 +0200

    API_Pipeline_a2

--- Data_pipeline_Tsla.py
+++ Data_pipeline_Tsla.py
@@ -2,9 +2,10 @@ import urllib3
 import json
 def get_data():
     http = urllib3.PoolManager()
-    url = "https://cloud.iexapis.com/stable/stock/tsla/previous?token=<YOUR_TOKEN_HERE>"
+    url = "https://cloud.iexapis.com/stable/stock/tsla/previous?token=<pk_99f064fa01784e02a0e518683fea021f>"
     resp = http.request("GET", url)
     values = json.loads(resp.data)
     print(values)
     return values
 get_data()
+
