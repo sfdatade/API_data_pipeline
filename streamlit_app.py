67841094dce875de9793f13b765f5d75a5a54b60
sfdatade <sfdatade@gmail.com>
2023-07-24 18:13:59 +0200

    API_Pipeline_a17

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,22 @@
 import gspread
 
+gc = gspread.service_account()
+
+# Open a sheet from a spreadsheet in one go
+wks = gc.open("CAB_P6_Data_pipeline_Tsla").sheet1
+
+# Update a range of cells using the top left corner address
+wks.update('A1', [[1, 2], [3, 4]])
+
+# Or update a single cell
+#wks.update('B42', "it's down there somewhere, let me take another look.")
+
+# Format the header
+wks.format('A1:B1', {'textFormat': {'bold': True}})
+
+
+
 def get_data():
     print("Hello World")
-
 get_data()
 
